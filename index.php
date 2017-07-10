d5f1e55ed39f2ccb49931d04fb99b3f66b8d36d0
Jerd22 <teeo300881@gmail.com>
2017-07-10 16:19:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <ul>
     <li><a href="https://coins.co.th/" target="_blank" >กระเป๋าหลัก</a></li>
     <li><a href="https://faucethub.io" target="_blank" >กระเป๋ากลาง</a></li>
-    <li><a href="https://www.instant-btc.eu/en" target="_blank" >Instant-BTC</a></li>
+    <li><a href="https://www.instant-btc.eu/ref/GEifZ8jebG" target="_blank" >Instant-BTC</a></li>
     <li><a href="https://claimwith.me/index.php" target="_blank" >Claim With Me</a></li>
     <li><a href="https://adsok.com/th" target="_blank" >adsOK</a></li>
     <li><a href="http://biterblog.com/?r=3FpRkqvJo6k7wd3ZLZMpFzKvnsavdFS9jM" target="_blank" >BIT Key</a></li>
