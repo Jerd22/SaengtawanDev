37f9add0369b7f4b4f0d539c359b0eff604c87e6
Jerd22 <teeo300881@gmail.com>
2017-07-10 15:46:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
 <title>เว็ปหาเงิน</title>
 </head>
 <body>
- <ul>
-    <li><a href="https://faucethub.io" targrt="new" >กระเป๋ากลาง</a></li>
-  </ul>
+<ul>
+    <li><a href="https://coins.co.th/" target="_blank" >กระเป๋าหลัก</a></li>
+    <li><a href="https://faucethub.io" target="_blank" >กระเป๋ากลาง</a></li>
+</ul>
 </body>
 </html>
