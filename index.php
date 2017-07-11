0fe4dfcef0673fdaad570484df2c84fd43e24c61
Jerd22 <teeo300881@gmail.com>
2017-07-11 15:42:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -15,5 +15,14 @@
     <li><a href="https://www.yourfreecoin.com/?r=3FpRkqvJo6k7wd3ZLZMpFzKvnsavdFS9jM" target="_blank" >Your Free 2</a></li>
     <li><a href="https://earnmoneyguide0.blogspot.com/p/free-bitcoin.html" target="_blank" >เก็บฟรี บิทคอยท์</a></li>
 </ul>
+    
+    <h6>Trade หุ้น</h6>
+  <ul>
+    <li><a href="https://traderider.com" target="_blank" >เว็ปกระทู้รายวัน</a></li>
+    <li><a href="https://www.xm.com/th/" target="_blank" >XM Trader</a></li>
+    <li><a href="https://www.exness.com/a/tdhujdhu" target="_blank" >exness Trader</a></li>
+    <li><a href="https://fbs.co.th/" target="_blank" >FBS Trader</a></li>
+    <li><a href="https://www.mql5.com/" target="_blank" >MQL5</a></li>
+</ul>  
 </body>
 </html>
